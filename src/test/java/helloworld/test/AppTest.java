5e8128994b911144dbaa68462e95fb49f17b1930
araghunatha2 <araghunatha2@csc.com>
2017-06-29 14:40:51 +0000

    If it doesn't work to be rolled back

---
+++ src/test/java/helloworld/test/AppTest.java
@@ -0,0 +1,24 @@
+package helloworld.test;
+
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AppTest {
+
+    @Before
+    public void prepare() {
+        //setBaseUrl("https://testhelloworld-javahelloworldweb.1d35.starter-us-east-1.openshiftapps.com");
+    }
+
+    @Test
+    public void testLogin() {
+        System.out.println("Test");
+        
+        //beginAt("/index");
+        //assertTitleEquals("Hello try again Java World!");        
+    }
+}
+
